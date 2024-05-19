0682f2fa3d1f67b4ab9b3f3b5525372d74e82cc2
Amos Kipyegon <dittoamos@gmail.com>
2024-05-19 10:47:29 +0300

    feat: home page content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "c-link": "#0F5A5E",
+      },
+    },
   },
   plugins: [],
 };
